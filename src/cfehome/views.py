8b30a749d85afd3421cca40d4e840e08e44427a5
Abhishek Late <abhisheklate8@gmail.com>
2026-05-20 18:58:48 +0530

    initial commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import h
+
+
+
+def home_page_view(request, *args, **kwargs):
+    my_title = "my page"
+    my_context = {
+        "page_title":my_title
+    }
+
+    html_template = "home.html"
+    print("path",request.path)
+    return render(request, html_template, my_context)
